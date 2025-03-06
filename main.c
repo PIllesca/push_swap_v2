11b82375e77124c9f79118680de2d854231848d9
pillesca <pillesca@student.42malaga.com>
2025-03-06 16:01:55 +0100

    2nd commit

---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 15:13:17 by root              #+#    #+#             */
+/*   Updated: 2025/03/06 15:45:14 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lib/push_swap.h"
+
+/**
+ * Body of the executable
+ * 
+ * @param[in] argc Number of arguments
+ * @param[in] argv String array input
+*/
+int	main(int argc, char *argv[])
+{
+	if (argc < 2)
+		exit(EXIT_FAILURE);
+	//ft_chk_args(argc, argv);
+	//ft_push_swap(argc, argv);
+	exit(EXIT_SUCCESS);
+}
